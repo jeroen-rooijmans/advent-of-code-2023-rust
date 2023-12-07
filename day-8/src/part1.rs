94b089227f246f399f1378f50e7e104887cd3a30
Jeroen Rooijmans <jeroenrooijmans@Jeroens-MacBook-Pro.local>
2023-12-08 12:15:50 +1300

    day-8 template

---
+++ day-8/src/part1.rs
@@ -0,0 +1,15 @@
+// Advent of Code - Day 8: Part 1
+
+pub(crate) fn solve_part_one(input: &str) -> u32 {
+    unimplemented!();
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn part1() {
+        let example_input = include_str!("./example.txt");
+        let answer = crate::part1::solve_part_one(example_input);
+        assert_eq!(answer, todo!());
+    }
+}
---
+++ day-8/src/part2.rs
@@ -0,0 +1,15 @@
+// Advent of Code - Day 8: Part 2
+
+pub(crate) fn solve_part_two(input: &str) -> u32 {
+    unimplemented!();
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn part2() {
+        let example_input = include_str!("./example.txt");
+        let answer = crate::part2::solve_part_two(example_input);
+        assert_eq!(answer, todo!());
+    }
+}
