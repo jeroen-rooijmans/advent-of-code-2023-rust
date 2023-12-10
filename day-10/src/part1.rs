c27cdd8be7bf4748faa4f5a9f962f0f6c2dda420
Jeroen Rooijmans <832720+jeroen-rooijmans@users.noreply.github.com>
2023-12-10 15:05:15 +1300

    day-10 template

---
+++ day-10/src/part1.rs
@@ -0,0 +1,15 @@
+// Advent of Code - Day 10: Part 1
+
+pub(crate) fn solve_part_one(input: &str) -> u32 {
+    unimplemented!();
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn part1() {
+        let example_input = "";
+        let answer = crate::part1::solve_part_one(example_input);
+        assert_eq!(answer, todo!());
+    }
+}
---
+++ day-10/src/part2.rs
@@ -0,0 +1,15 @@
+// Advent of Code - Day 10: Part 2
+
+pub(crate) fn solve_part_two(input: &str) -> u32 {
+    unimplemented!();
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn part1() {
+        let example_input = "";
+        let answer = crate::part2::solve_part_two(example_input);
+        assert_eq!(answer, todo!());
+    }
+}
