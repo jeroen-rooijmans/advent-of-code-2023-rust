23c0a129c7b58ec304bb84fc56910f9700fff207
Jeroen Rooijmans <832720+jeroen-rooijmans@users.noreply.github.com>
2025-12-07 22:26:50 +0100

    day-24

---
+++ day-24/src/part1.rs
@@ -0,0 +1,122 @@
+// Advent of Code - Day 24: Never Tell Me The Odds Part 1
+use std::{ops::RangeInclusive, str::FromStr};
+
+#[allow(dead_code)]
+#[derive(Clone, Copy)]
+struct Hailstone {
+    px: f64,
+    py: f64,
+    pz: f64,
+    vx: f64,
+    vy: f64,
+    vz: f64,
+}
+
+#[derive(Debug)]
+struct ParseHailstoneError;
+
+impl FromStr for Hailstone {
+    type Err = ParseHailstoneError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.replace('@', ",").replace(' ', "");
+        let mut parts = s.split(',');
+        let px = parts
+            .next()
+            .unwrap()
+            .parse::<f64>()
+            .map_err(|_| ParseHailstoneError)?;
+        let py = parts
+            .next()
+            .unwrap()
+            .parse::<f64>()
+            .map_err(|_| ParseHailstoneError)?;
+        let pz = parts
+            .next()
+            .unwrap()
+            .parse::<f64>()
+            .map_err(|_| ParseHailstoneError)?;
+        let vx = parts
+            .next()
+            .unwrap()
+            .parse::<f64>()
+            .map_err(|_| ParseHailstoneError)?;
+        let vy = parts
+            .next()
+            .unwrap()
+            .parse::<f64>()
+            .map_err(|_| ParseHailstoneError)?;
+        let vz = parts
+            .next()
+            .unwrap()
+            .parse::<f64>()
+            .map_err(|_| ParseHailstoneError)?;
+        Ok(Hailstone {
+            px,
+            py,
+            pz,
+            vx,
+            vy,
+            vz,
+        })
+    }
+}
+
+impl Hailstone {
+    fn intersect(self, other: &Self) -> Option<(f64, f64)> {
+        let det = (self.vy * other.vx) - (self.vx * other.vy);
+        if det == 0.0 {
+            return None;
+        };
+
+        let t1 = (other.vx * (other.py - self.py) - other.vy * (other.px - self.px)) / det;
+        let t2 = (self.vx * (other.py - self.py) - self.vy * (other.px - self.px)) / det;
+        if [t1, t2].iter().any(|t| t.is_sign_negative()) {
+            return None;
+        };
+
+        let x_intersect = self.px + t1 * self.vx;
+        let y_intersect = self.py + t1 * self.vy;
+
+        Some((x_intersect, y_intersect))
+    }
+}
+
+fn combinations<T>(vec: &[T]) -> Vec<(usize, usize)>
+where
+    T: Clone,
+{
+    let mut combinations = Vec::new();
+    for i in 0..vec.len() {
+        for j in (i + 1)..vec.len() {
+            combinations.push((i, j));
+        }
+    }
+    combinations
+}
+
+pub(crate) fn solve(input: &str, test_area: RangeInclusive<f64>) -> usize {
+    let hailstones = input
+        .lines()
+        .map(|l| Hailstone::from_str(l).unwrap())
+        .collect::<Vec<_>>();
+    combinations(&hailstones)
+        .iter()
+        .filter_map(|&(f, s)| hailstones[f].intersect(&hailstones[s]))
+        .filter(|(x, y)| test_area.contains(x) && test_area.contains(y))
+        .count()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn part1() {
+        let example_input = "19, 13, 30 @ -2,  1, -2
+18, 19, 22 @ -1, -1, -2
+20, 25, 34 @ -2, -2, -4
+12, 31, 28 @ -1, -2, -1
+20, 19, 15 @  1, -5, -3";
+        let answer = crate::part1::solve(example_input, 7.0..=27.0);
+        assert_eq!(answer, 2);
+    }
+}
---
+++ day-24/src/part2.rs
@@ -0,0 +1,201 @@
+// Advent of Code - Day 24: Never Tell Me The Odds Part 2
+use std::ops::{Add, Mul, Sub};
+
+use num_bigint::BigInt;
+
+type Coord = BigInt;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Vector3d {
+    x: Coord,
+    y: Coord,
+    z: Coord,
+}
+impl Add<&Vector3d> for &Vector3d {
+    type Output = Vector3d;
+    fn add(self, other: &Vector3d) -> Self::Output {
+        Vector3d {
+            x: self.x.clone() + &other.x,
+            y: self.y.clone() + &other.y,
+            z: self.z.clone() + &other.z,
+        }
+    }
+}
+
+impl Sub<&Vector3d> for &Vector3d {
+    type Output = Vector3d;
+    fn sub(self, other: &Vector3d) -> Self::Output {
+        Vector3d {
+            x: self.x.clone() - &other.x,
+            y: self.y.clone() - &other.y,
+            z: self.z.clone() - &other.z,
+        }
+    }
+}
+
+impl Mul<&Vector3d> for &Vector3d {
+    type Output = Vector3d;
+    fn mul(self, other: &Vector3d) -> Self::Output {
+        Vector3d {
+            x: self.x.clone() * &other.x,
+            y: self.y.clone() * &other.y,
+            z: self.z.clone() * &other.z,
+        }
+    }
+}
+
+impl Vector3d {
+    /// Cross Product: A x B
+    fn cross(&self, other: &Self) -> Self {
+        Vector3d {
+            x: self.y.clone() * &other.z - self.z.clone() * &other.y,
+            y: self.z.clone() * &other.x - self.x.clone() * &other.z,
+            z: self.x.clone() * &other.y - self.y.clone() * &other.x,
+        }
+    }
+
+    /// Dot Product: A . B
+    fn dot(&self, other: &Self) -> Coord {
+        self.x.clone() * &other.x + self.y.clone() * &other.y + self.z.clone() * &other.z
+    }
+}
+
+struct Hailstone {
+    p: Vector3d,
+    v: Vector3d,
+}
+
+// A_ij = (V_i - V_j) x (P_i - P_j)
+fn get_a_vector(h1: &Hailstone, h2: &Hailstone) -> Vector3d {
+    let dv = &h1.v - &h2.v;
+    let dp = &h1.p - &h2.p;
+    dv.cross(&dp)
+}
+
+// B_ij = (V_i - V_j) . (P_i x P_j)
+fn get_b_scalar(h1: &Hailstone, h2: &Hailstone) -> Coord {
+    let dv = &h1.v - &h2.v;
+    let cross_p = h1.p.cross(&h2.p);
+    dv.dot(&cross_p)
+}
+
+/// Calculates the determinant of 3d matrix
+/// | a b c |
+/// | d e f | = a(ei - fh) - b(di - fg) + c(dh - eg)
+/// | g h i |
+fn det3d(m: &[[Coord; 3]; 3]) -> Coord {
+    m[0][0].clone() * (m[1][1].clone() * &m[2][2] - m[1][2].clone() * &m[2][1])
+        - m[0][1].clone() * (m[1][0].clone() * &m[2][2] - m[1][2].clone() * &m[2][0])
+        + m[0][2].clone() * (m[1][0].clone() * &m[2][1] - m[1][1].clone() * &m[2][0])
+}
+
+fn gaussian_elimination_solver(m: &[[Coord; 3]; 3], b: &[Coord; 3]) -> Vector3d {
+    let det_m = det3d(m);
+    if det_m == BigInt::from(0) {
+        panic!("Cannot solve singular matrix (det=0)");
+    }
+
+    // Solve for Px
+    let mut mx = m.clone();
+    mx[0][0] = b[0].clone();
+    mx[1][0] = b[1].clone();
+    mx[2][0] = b[2].clone();
+    let det_mx = det3d(&mx);
+
+    // Solve for Py
+    let mut my = m.clone();
+    my[0][1] = b[0].clone();
+    my[1][1] = b[1].clone();
+    my[2][1] = b[2].clone();
+    let det_my = det3d(&my);
+
+    // Solve for Pz
+    let mut mz = m.clone();
+    mz[0][2] = b[0].clone();
+    mz[1][2] = b[1].clone();
+    mz[2][2] = b[2].clone();
+    let det_mz = det3d(&mz);
+
+    // Divide by original determinant to get solution
+    Vector3d {
+        x: det_mx / &det_m,
+        y: det_my / &det_m,
+        z: det_mz / &det_m,
+    }
+}
+
+fn parse_input(input: &str) -> Vec<Hailstone> {
+    input
+        .lines()
+        .filter_map(|line| {
+            let parts: Vec<&str> = line.split('@').collect();
+            // Function to parse the 3 coordinates (Px, Py, Pz) or (Vx, Vy, Vz)
+            let parse_coords = |s: &str| -> Option<Vector3d> {
+                let coords: Vec<i128> = s
+                    .split(',')
+                    .map(|c| c.trim().parse::<i128>())
+                    .filter_map(Result::ok)
+                    .collect();
+
+                if coords.len() == 3 {
+                    Some(Vector3d {
+                        x: coords[0].into(),
+                        y: coords[1].into(),
+                        z: coords[2].into(),
+                    })
+                } else {
+                    None
+                }
+            };
+
+            let p_vec = parse_coords(parts[0])?;
+            let v_vec = parse_coords(parts[1])?;
+
+            Some(Hailstone { p: p_vec, v: v_vec })
+        })
+        .collect()
+}
+
+pub(crate) fn solve(input: &str) -> BigInt {
+    let hailstones = parse_input(input);
+    // use first 3 hailstones to from pairs (0,1), (0,2) and (1,2)
+    let hailstone_0 = &hailstones[0];
+    let hailstone_1 = &hailstones[1];
+    let hailstone_2 = &hailstones[2];
+
+    let a01 = get_a_vector(hailstone_0, hailstone_1);
+    let b01 = get_b_scalar(hailstone_0, hailstone_1);
+    let a02 = get_a_vector(hailstone_0, hailstone_2);
+    let b02 = get_b_scalar(hailstone_0, hailstone_2);
+    let a12 = get_a_vector(hailstone_1, hailstone_2);
+    let b12 = get_b_scalar(hailstone_1, hailstone_2);
+
+    // construct 3x3 coefficient matrix M
+    let m = [
+        [a01.x, a01.y, a01.z],
+        [a02.x, a02.y, a02.z],
+        [a12.x, a12.y, a12.z],
+    ];
+    // 3x1 constant vector B
+    let b = [b01, b02, b12];
+
+    // Solve for rock initial position P
+    let p = gaussian_elimination_solver(&m, &b);
+
+    // sum coordinates of rock's initial position
+    p.x + p.y + p.z
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn part2() {
+        let example_input = "19, 13, 30 @ -2,  1, -2
+18, 19, 22 @ -1, -1, -2
+20, 25, 34 @ -2, -2, -4
+12, 31, 28 @ -1, -2, -1
+20, 19, 15 @  1, -5, -3";
+        let answer = crate::part2::solve(example_input);
+        assert_eq!(answer, num_bigint::BigInt::from(47));
+    }
+}
